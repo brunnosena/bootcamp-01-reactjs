c51e8cc942f48c70bbb33f471404701f9a66aac2
Brunno Sena <brunnow@hotmail.com>
2020-06-16 10:14:35 -0300

    Complete second challenge GoStack [ReactJS]

--- src/App.js
+++ src/App.js
@@ -1,27 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
+import api from './services/api'
 
 function App() {
+  const [repository, setRepository] = useState([])
+  const [title, setTitle] = useState('')
+  const [url, setUrl] = useState('https://github.com/')
+  const [techs, setTechs] = useState('')
+  
+  useEffect(() => {
+    const getRepositories = async () => {
+      await api.get('/repositories')
+      .then(res => setRepository(res.data))
+      .catch(err => console.log(`Erro ao retornar dados: ${err}`))
+    }
+
+    getRepositories()       
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const techsInArray = techs.trim().split(', ');
+
+    await api.post('/repositories', {
+      title,
+      url,
+      techs: techsInArray
+    })
+    .then(res => setRepository([...repository, res.data]))
+    .catch(err => console.log(`Erro ao add dados: ${err}`))
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`)
+      .then(() => {
+        const novo = repository.filter(function(el) { 
+          return el.id !== id; 
+        });
+
+        setRepository(novo)
+      })
+      .catch(err => console.log(`Erro ao deletar dados: ${err}`))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {
+          repository.map(repo => (
+            <li key={repo.id}>
+              {repo.title}
+              <button onClick={() => handleRemoveRepository(repo.id)}>
+                Remover
+              </button>
+            </li>
+          ))
+        }
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
       </ul>
+
+      <form>
+        <input type="text"
+        placeholder="Type the name"
+        value={title}
+        onChange={e => setTitle(e.target.value)}/>
+        <input type="text"
+        placeholder="Type the url"
+        value={url}
+        onChange={e => setUrl(e.target.value)}/>
+        <input type="text"
+        placeholder="Type the techs separated by comma"
+        value={techs}
+        onChange={e => setTechs(e.target.value)}/>
+      </form>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
